ed9e765ffa17fb4bfa4259292d4a8b89959df502
gurunelee <dlckdgk4858@gmail.com>
2020-10-19 22:03:39 +0900

    v1.0 face-ai-server

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 	"net/http"
 	"os"
 
-	goFace "github.com/Kagami/go-face"
+	"errors"
+
+	"github.com/Kagami/go-face"
 	"github.com/gorilla/mux"
 )
 
 type Response struct {
-	Feature goFace.Descriptor `json:"feature"`
-	Message string            `json:"msg" `
-	Err     error             `json:"error"`
+	Feature face.Descriptor `json:"feature"`
+	Message string          `json:"msg" `
+	Err     string          `json:"error"`
 }
 
 const (
@@ -26,60 +28,94 @@ const (
 func main() {
 	fmt.Println("server start")
 	router := mux.NewRouter()
-
 	router.HandleFunc("/get/feature", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("client entering")
 
 		//parsing
 		imgBin, err := getImg(r)
 		if err != nil {
-			fmt.Println("getImg error, Erro: ", fmt.Sprint(err))
+			fmt.Errorf("getImg error, Error: ", err.Error())
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			var nilDes face.Descriptor
+			failData := Response{nilDes, ErrMsg, err.Error()}
+			failBytes, err := json.Marshal(failData)
+			if err != nil {
+				fmt.Errorf("marshaling error, Erro: ", err.Error())
+				return
+			}
+			w.Write(failBytes)
 			return
 		}
 
 		//get feature
 		descriptor, err := getFeature(imgBin)
 		if err != nil {
-			fmt.Println("getFeature error, Erro: ", fmt.Sprint(err))
+			fmt.Println("getFeature error, Erro: ", err.Error())
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			failData := Response{descriptor, ErrMsg, err.Error()}
+			failBytes, err := json.Marshal(failData)
+			if err != nil {
+				fmt.Errorf("marshaling error, Erro: ", err.Error())
+				return
+			}
+			w.Write(failBytes)
 			return
 		}
-		featureData := Response{descriptor, SuccessMsg, nil}
-		featureBytes, _ := json.Marshal(featureData)
-
+		featureData := Response{descriptor, SuccessMsg, ""}
+		featureBytes, err := json.Marshal(featureData)
+		if err != nil {
+			fmt.Errorf("marshaling error, Erro: ", err.Error())
+			return
+		}
 		//response
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusAccepted)
 		w.Write(featureBytes)
 
-	}).Methods(http.MethodGet)
+	}).Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func getImg(r *http.Request) ([]byte, error) {
+	r.ParseMultipartForm(32 << 20)
 	file, _, err := r.FormFile("face-img")
 	if err != nil {
 		var emptyslice []byte
 		return emptyslice, err
 	}
 	defer file.Close()
+
 	imgByte, err := ioutil.ReadAll(file)
-	return imgByte, nil
+	if err != nil {
+		var emptyslice []byte
+		return emptyslice, err
+	}
+	return imgByte, err
 }
 
-func getFeature(bin []byte) (goFace.Descriptor, error) {
-	modleDir := os.Getenv("MODEL_DIR")
+func getFeature(bin []byte) (face.Descriptor, error) {
+	modelDir := os.Getenv("MODEL_DIR")
 
-	var nilDes goFace.Descriptor
-	rec, err := goFace.NewRecognizer(modleDir)
+	var nilDes face.Descriptor
+	rec, err := face.NewRecognizer(modelDir)
 	if err != nil {
 		return nilDes, err
 	}
-	face, err := rec.RecognizeSingle(bin)
-	if err != nil {
-		return nilDes, err
-	}
+	defer rec.Close()
 
-	return face.Descriptor, nil
+	//this src can recognize only 'one-face'image
+	f, err := rec.RecognizeSingle(bin)
+	if f == nil {
+		if err != nil {
+			return nilDes, err
+		} else {
+			fmt.Errorf("There is no face on requested img\n")
+			return nilDes, errors.New("There is no face on requested img")
+		}
+	}
+	return f.Descriptor, err
 
 }
